163c17ae12e2d345deb87d0cf4dcc0c876392678
T. Munawar Khalil <tmunawar.k@mhs.unsyiah.ac.id>
2022-10-26 10:42:07 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/al_quran_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.al_quran_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:al_quran_app/presentation/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
