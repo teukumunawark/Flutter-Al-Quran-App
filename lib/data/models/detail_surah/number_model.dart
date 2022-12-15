6010a54fc08ea7f14cc66ef5221e13fa29db1468
teukumunawark <munawartmk@gmail.com>
2022-12-15 17:30:58 +0700

    models update

---
+++ lib/data/models/detail_surah/number_model.dart
@@ -0,0 +1,30 @@
+import 'package:al_quran_app/domain/entities/surah_detail_entities.dart';
+import 'package:equatable/equatable.dart';
+
+class Number extends Equatable {
+  final int? inQuran;
+  final int? inSurah;
+
+  const Number({this.inQuran, this.inSurah});
+
+  factory Number.fromJson(Map<String, dynamic> json) => Number(
+        inQuran: json['inQuran'] as int?,
+        inSurah: json['inSurah'] as int?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'inQuran': inQuran,
+        'inSurah': inSurah,
+      };
+
+  NumberEntities toEntity() => NumberEntities(
+        inQuran: inQuran,
+        inSurah: inSurah,
+      );
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object?> get props => [inQuran, inSurah];
+}
---
+++ lib/data/models/detail_surah/revelation_model.dart
@@ -0,0 +1,34 @@
+import 'package:al_quran_app/domain/entities/surah_detail_entities.dart';
+import 'package:equatable/equatable.dart';
+
+class Revelation extends Equatable {
+  final String? arab;
+  final String? en;
+  final String? id;
+
+  const Revelation({this.arab, this.en, this.id});
+
+  factory Revelation.fromJson(Map<String, dynamic> json) => Revelation(
+        arab: json['arab'] as String?,
+        en: json['en'] as String?,
+        id: json['id'] as String?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'arab': arab,
+        'en': en,
+        'id': id,
+      };
+
+  RevelationEntities toEntity() => RevelationEntities(
+        arab: arab,
+        en: en,
+        id: id,
+      );
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object?> get props => [arab, en, id];
+}
