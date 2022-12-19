8ccc5f1e1c45dd2663b83a763ad7e1fa7a29816c
teukumunawark <munawartmk@gmail.com>
2022-12-19 22:25:10 +0700

    add detail bloc

--- lib/presentation/bloc/quran_bloc.dart
+++ lib/presentation/bloc/quran_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:al_quran_app/domain/usecases/get_detail_surah.dart';
 import 'package:al_quran_app/domain/usecases/get_surah_list.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,7 +8,7 @@ import '../../domain/entities/list_surah_entities/surah_entities.dart';
 part 'quran_event.dart';
 part 'quran_state.dart';
 
-class SurahListBloc extends Bloc<QuranEvent, QuranState> {
+class SurahListBloc extends Bloc<SurahEvent, SurahState> {
   final GetSurahList _getQuranList;
 
   SurahListBloc(this._getQuranList) : super(QuranLoading()) {
@@ -17,6 +18,24 @@ class SurahListBloc extends Bloc<QuranEvent, QuranState> {
       result.fold(
         (failure) => emit(QuranError(failure.message)),
         (data) => emit(QuranHasData(data)),
+      );
+    });
+  }
+}
+
+class DetailSurahBloc extends Bloc<SurahEvent, SurahState> {
+  final GetDetailSurah _getDetailSurah;
+
+  DetailSurahBloc(this._getDetailSurah) : super(QuranLoading()) {
+    on<OnDetailSurah>((event, emit) async {
+      final id = event.id;
+
+      emit(QuranLoading());
+
+      final result = await _getDetailSurah.execute(id);
+      result.fold(
+        (l) => null,
+        (r) => null,
       );
     });
   }
--- lib/presentation/bloc/quran_event.dart
+++ lib/presentation/bloc/quran_event.dart
@@ -1,10 +1,19 @@
 part of 'quran_bloc.dart';
 
-abstract class QuranEvent extends Equatable {
-  const QuranEvent();
+abstract class SurahEvent extends Equatable {
+  const SurahEvent();
 
   @override
   List<Object> get props => [];
 }
 
-class OnQuranList extends QuranEvent {}
+class OnQuranList extends SurahEvent {}
+
+class OnDetailSurah extends SurahEvent {
+  final int id;
+
+  const OnDetailSurah(this.id);
+
+  @override
+  List<Object> get props => [id];
+}
