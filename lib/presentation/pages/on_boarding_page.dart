85916af48a355df72e3a94161433bcc8691373e5
teukumunawark <munawartmk@gmail.com>
2022-12-29 22:39:39 +0700

    done onboarding page

--- lib/presentation/pages/on_boarding_page.dart
+++ lib/presentation/pages/on_boarding_page.dart
@@ -1,16 +1,19 @@
 import 'package:al_quran_app/common/constants.dart';
 import 'package:al_quran_app/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie/lottie.dart';
 
 class OnBoardingPage extends StatelessWidget {
-  static const routeName = '/';
+  static const routeName = '/onboard-page';
 
   const OnBoardingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+    final sizeAppBar = MediaQuery.of(context).padding.top;
+    final height = MediaQuery.of(context).size.height - sizeAppBar;
 
     return Scaffold(
       body: SafeArea(
@@ -20,7 +23,7 @@ class OnBoardingPage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(height: size.height * 0.04),
+                SizedBox(height: height * 0.05),
                 Text(
                   "Quran App",
                   style: kHeading4.copyWith(color: kMeteorite),
@@ -33,20 +36,34 @@ class OnBoardingPage extends StatelessWidget {
                 ),
                 const Spacer(),
                 SizedBox(
-                  height: 480,
+                  height: height * 0.65,
                   child: Stack(
                     children: [
                       Container(
-                        height: 450,
-                        width: size.width,
-                        decoration: BoxDecoration(
-                          color: kMeteorite,
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: SvgPicture.asset(
-                          'assets/svg/onboard-img.svg',
-                        ),
-                      ),
+                          height: height * 0.615,
+                          width: size.width,
+                          decoration: BoxDecoration(
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color(0xffDF98FA),
+                                Color(0xff9055FF),
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Stack(
+                            children: [
+                              SvgPicture.asset('assets/svg/onboard-img.svg'),
+                              Positioned(
+                                top: 100,
+                                left: 5,
+                                right: 5,
+                                child: Lottie.asset('assets/lottie/quran.json'),
+                              ),
+                            ],
+                          )),
                       Align(
                         alignment: Alignment.bottomCenter,
                         child: Material(
