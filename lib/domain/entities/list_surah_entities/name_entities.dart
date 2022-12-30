0cd0299a3ef62a7ad7ba4225432af9653693a064
teukumunawark <munawartmk@gmail.com>
2022-12-30 22:53:36 +0700

    make entitie object as nullable

--- lib/domain/entities/list_surah_entities/name_entities.dart
+++ lib/domain/entities/list_surah_entities/name_entities.dart
@@ -3,20 +3,20 @@ import 'package:equatable/equatable.dart';
 import 'translation_entities.dart';
 
 class NameEntities extends Equatable {
-  final String short;
-  final String long;
-  final TranslationEntities translation;
-  final TranslationEntities transliteration;
+  final String? short;
+  final String? long;
+  final TranslationEntities? translation;
+  final TranslationEntities? transliteration;
 
   const NameEntities({
-    required this.short,
-    required this.long,
-    required this.translation,
-    required this.transliteration,
+    this.short,
+    this.long,
+    this.translation,
+    this.transliteration,
   });
 
   @override
-  List<Object> get props => [
+  List<Object?> get props => [
         short,
         long,
         transliteration,
--- lib/domain/entities/list_surah_entities/revelation_entities.dart
+++ lib/domain/entities/list_surah_entities/revelation_entities.dart
@@ -1,18 +1,18 @@
 import 'package:equatable/equatable.dart';
 
 class RevelationEntities extends Equatable {
-  final String arab;
-  final String en;
-  final String id;
+  final String? arab;
+  final String? en;
+  final String? id;
 
   const RevelationEntities({
-    required this.arab,
-    required this.en,
-    required this.id,
+    this.arab,
+    this.en,
+    this.id,
   });
 
   @override
-  List<Object> get props => [
+  List<Object?> get props => [
         arab,
         en,
         id,
