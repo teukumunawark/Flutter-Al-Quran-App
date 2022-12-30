b9b2d7b59183be0ac1718068e0598b302718215f
teukumunawark <munawartmk@gmail.com>
2022-12-30 22:53:41 +0700

    make entitie object as nullable

--- lib/domain/entities/list_surah_entities/surah_entities.dart
+++ lib/domain/entities/list_surah_entities/surah_entities.dart
@@ -5,24 +5,24 @@ import 'revelation_entities.dart';
 import 'tafsir_id_entities.dart';
 
 class SurahEntities extends Equatable {
-  final int number;
-  final int sequence;
-  final int numberOfVerses;
-  final NameEntities name;
-  final RevelationEntities revelation;
-  final TafsirEntities tafsir;
+  final int? number;
+  final int? sequence;
+  final int? numberOfVerses;
+  final NameEntities? name;
+  final RevelationEntities? revelation;
+  final TafsirEntities? tafsir;
 
   const SurahEntities({
-    required this.number,
-    required this.sequence,
-    required this.numberOfVerses,
-    required this.name,
-    required this.revelation,
-    required this.tafsir,
+    this.number,
+    this.sequence,
+    this.numberOfVerses,
+    this.name,
+    this.revelation,
+    this.tafsir,
   });
 
   @override
-  List<Object> get props => [
+  List<Object?> get props => [
         number,
         sequence,
         numberOfVerses,
