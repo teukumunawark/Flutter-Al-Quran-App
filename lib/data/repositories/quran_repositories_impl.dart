bac3f2199230f56466831e936f9c8efcb4a4af28
teukumunawark <munawartmk@gmail.com>
2023-01-02 11:32:25 +0700

    update

--- lib/data/repositories/quran_repositories_impl.dart
+++ lib/data/repositories/quran_repositories_impl.dart
@@ -1,20 +1,19 @@
 import 'dart:io';
+import 'package:al_quran_app/domain/entities/detail_juz_entities/detail_juz_entities.dart';
 import 'package:dartz/dartz.dart';
 
-import '../../common/exception.dart';
 import '../../common/failure.dart';
-import '../../domain/entities/detail_surah_entities/detail_surah_entities.dart';
+import '../../common/exception.dart';
+import '../datasources/quran_data_source.dart';
+import '../../domain/repositories/quran_repository.dart';
 import '../../domain/entities/list_juz_entities/juz_entities.dart';
 import '../../domain/entities/list_surah_entities/surah_entities.dart';
-import '../../domain/repositories/quran_repository.dart';
-import '../datasources/quran_data_source.dart';
+import '../../domain/entities/detail_surah_entities/detail_surah_entities.dart';
 
 class SurahRepositoryImpl implements SurahRepository {
   final SurahRemoteDataSource remoteDataSource;
 
-  SurahRepositoryImpl({
-    required this.remoteDataSource,
-  });
+  SurahRepositoryImpl({required this.remoteDataSource});
 
   @override
   Future<Either<Failure, List<SurahEntities>>> getListSurah() async {
@@ -23,8 +22,6 @@ class SurahRepositoryImpl implements SurahRepository {
       return Right(result.map((model) => model.toEntity()).toList());
     } on ServerException {
       return Left(ServerFailure(''));
-    } on SocketException {
-      return Left(ConnectionFailure('Failed to connect to the network'));
     }
   }
 
@@ -35,15 +32,23 @@ class SurahRepositoryImpl implements SurahRepository {
       return Right(result.map((model) => model.toEntity()).toList());
     } on ServerException {
       return Left(ServerFailure(''));
-    } on SocketException {
-      return Left(ConnectionFailure('Failed to connect to the network'));
     }
   }
 
   @override
-  Future<Either<Failure, DetailSurahEntities>> getDetailSurah(int id) async {
+  Future<Either<Failure, DetailJuzEntities>> getDetailJuz(String id) async {
     try {
-      final result = await remoteDataSource.getDetailSurah(id);
+      final result = await remoteDataSource.readDetailJuz(id);
+      return Right(result.toEntity());
+    } on ServerException {
+      return Left(ServerFailure(''));
+    }
+  }
+
+  @override
+  Future<Either<Failure, DetailSurahEntities>> getDetailSurah(String id) async {
+    try {
+      final result = await remoteDataSource.readDetailSurah(id);
       return Right(result.toEntity());
     } on ServerException {
       return Left(ServerFailure(''));
