20c21ef4cd07194971ad4312a1f6f43a7ec1b05e
teukumunawark <munawartmk@gmail.com>
2023-01-04 20:14:20 +0700

    adding jadwal shalat bloc

--- lib/presentation/bloc/quran_bloc.dart
+++ lib/presentation/bloc/quran_bloc.dart
@@ -1,14 +1,16 @@
-import 'package:al_quran_app/domain/entities/detail_juz_entities/detail_juz_entities.dart';
-import 'package:al_quran_app/domain/entities/detail_surah_entities/detail_surah_entities.dart';
-import 'package:al_quran_app/domain/entities/list_juz_entities/juz_entities.dart';
+import 'package:al_quran_app/domain/entities/jadwal_entities/jadwal_shalat_entities.dart';
+import 'package:al_quran_app/domain/entities/juz_entities/detail_juz_entities.dart';
+import 'package:al_quran_app/domain/entities/surah_entities/detail_surah_entities/detail_surah_entities.dart';
+import 'package:al_quran_app/domain/entities/juz_entities/juz_entities.dart';
 import 'package:al_quran_app/domain/usecases/get_detail_juz.dart';
 import 'package:al_quran_app/domain/usecases/get_detail_surah.dart';
+import 'package:al_quran_app/domain/usecases/get_jadwal_shalat.dart';
 import 'package:al_quran_app/domain/usecases/get_juz_list.dart';
 import 'package:al_quran_app/domain/usecases/get_surah_list.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../domain/entities/list_surah_entities/surah_entities.dart';
+import '../../domain/entities/surah_entities/list_surah_entities/surah_entities.dart';
 
 part 'quran_event.dart';
 part 'quran_state.dart';
@@ -21,7 +23,7 @@ class SurahListBloc extends Bloc<QuranEvent, QuranState> {
       emit(QuranLoading());
       final result = await _getQuranList.execute();
       result.fold(
-        (failure) => emit(SurahError(failure.message)),
+        (failure) => emit(QuranError(failure.message)),
         (data) => emit(SurahHasData(data)),
       );
     });
@@ -36,7 +38,7 @@ class JuzListBloc extends Bloc<QuranEvent, QuranState> {
       emit(QuranLoading());
       final result = await _getJuzList.execute();
       result.fold(
-        (failure) => emit(SurahError(failure.message)),
+        (failure) => emit(QuranError(failure.message)),
         (data) => emit(JuzHasData(data)),
       );
     });
@@ -52,7 +54,7 @@ class DetailSurahBloc extends Bloc<QuranEvent, QuranState> {
       emit(QuranLoading());
       final result = await _getDetailSurah.execute(id);
       result.fold(
-        (failure) => emit(SurahError(failure.message)),
+        (failure) => emit(QuranError(failure.message)),
         (data) => emit(DetailSurahHasData(data)),
       );
     });
@@ -68,8 +70,24 @@ class DetailJuzBloc extends Bloc<QuranEvent, QuranState> {
       emit(QuranLoading());
       final result = await _getDetailJuz.execute(id);
       result.fold(
-        (failure) => emit(SurahError(failure.message)),
+        (failure) => emit(QuranError(failure.message)),
         (data) => emit(DetailJuzHasData(data)),
+      );
+    });
+  }
+}
+
+class JadwalShalatBloc extends Bloc<QuranEvent, QuranState> {
+  final GetjadwalShalat _getjadwalShalat;
+
+  JadwalShalatBloc(this._getjadwalShalat) : super(QuranLoading()) {
+    on<OnJadwalShalat>((event, emit) async {
+      final month = event.month;
+      emit(QuranLoading());
+      final result = await _getjadwalShalat.execute(month);
+      result.fold(
+        (failure) => emit(QuranError(failure.message)),
+        (data) => emit(JadwalHasData(data)),
       );
     });
   }
