cb39a3a92b8a278ea7943326942d05956f7bf9a6
teukumunawark <munawartmk@gmail.com>
2023-01-04 20:13:46 +0700

    adding jadwal shalat repo

--- lib/data/repositories/quran_repositories_impl.dart
+++ lib/data/repositories/quran_repositories_impl.dart
@@ -1,14 +1,17 @@
+import 'dart:io';
+
+import 'package:al_quran_app/domain/entities/jadwal_entities/jadwal_shalat_entities.dart';
 import 'package:dartz/dartz.dart';
 import '../../common/failure.dart';
 import '../../common/exception.dart';
-import '../../domain/entities/detail_juz_entities/detail_juz_entities.dart';
+import '../../domain/entities/juz_entities/detail_juz_entities.dart';
 import '../datasources/quran_data_source.dart';
 import '../../domain/repositories/quran_repository.dart';
-import '../../domain/entities/list_juz_entities/juz_entities.dart';
-import '../../domain/entities/list_surah_entities/surah_entities.dart';
-import '../../domain/entities/detail_surah_entities/detail_surah_entities.dart';
+import '../../domain/entities/juz_entities/juz_entities.dart';
+import '../../domain/entities/surah_entities/list_surah_entities/surah_entities.dart';
+import '../../domain/entities/surah_entities/detail_surah_entities/detail_surah_entities.dart';
 
-class SurahRepositoryImpl implements SurahRepository {
+class SurahRepositoryImpl implements QuranRepository {
   final SurahRemoteDataSource remoteDataSource;
 
   SurahRepositoryImpl({required this.remoteDataSource});
@@ -50,6 +53,19 @@ class SurahRepositoryImpl implements SurahRepository {
       return Right(result.toEntity());
     } on ServerException {
       return Left(ServerFailure(''));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<JadwalEntities>>> getJadwalShalat(
+      String month) async {
+    try {
+      final result = await remoteDataSource.getJadwalShalat(month);
+      return Right(result.map((model) => model.toEntity()).toList());
+    } on ServerException {
+      return Left(ServerFailure(''));
+    } on SocketException {
+      return Left(ConnectionFailure('Failed to connect to the network'));
     }
   }
 }
